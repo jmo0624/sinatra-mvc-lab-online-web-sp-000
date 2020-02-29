5f2b8a5c2ce2205488524d2fbce317f13d6e44d0
Josh Morris <jmo0624@gmail.com>
2020-02-29 20:17:26 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,32 +1,69 @@
-
 class PigLatinizer
-  attr_accessor :phrase	
 
-  def initialize(phrase)	
-    @phrase = phrase	
-  end	
-
-
-  def is_vowel?(word)
-    vowels = 'aeiouAEIOU'
-    vowels.include?(word[0])	
+  def piglatinize(user_phrase)
+    @new_word = user_phrase.split("")
+    vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+    if vowels.include?(@new_word[0])
+      latinized_word = begins_with_vowel.join("")
+    else
+      latinized_word = begins_with_consonant.join("")
+    end
+    latinized_word
   end
 
+  def to_pig_latin(sentence)
+    words_in_sentence = sentence.split(" ")
 
-  def piglatinize(word)
-    if self.is_vowel?(word)
-      word.concat('way')
+    pig_latinized_words = words_in_sentence.collect do |word|
+      self.piglatinize(word)
+    end
+
+    new_sentence = pig_latinized_words.join(" ")
+  end
+
+  def begins_with_vowel
+    vowel_word = @new_word << "way"
+  end
+
+  def begins_with_consonant
+    if @new_word[0] == "q" && @new_word[1] == "u"
+      qu = @new_word[0, 2].join("")
+      spliced_word = @new_word[2..-1]
+      spliced_word << qa + "ay"
+    elsif @new_word[0] == "p" && @new_word[1] =="l"
+      pl = @new_word[0, 2].join("")
+      spliced_word = @new_word[2..-1]
+      spliced_word << pl + "ay"
+    elsif @new_word[0] == "t" && @new_word[1] =="h"
+      th = @new_word[0, 2].join("")
+      spliced_word = @new_word[2..-1]
+      spliced_word << th + "ay"
+    elsif @new_word[0] == "s" && @new_word[1] =="p" && @new_word[2] == "r"
+      spr = @new_word[0, 3].join("")
+      spliced_word = @new_word[3..-1]
+      spliced_word << spr + "ay"
+    elsif @new_word[0] == "p" && @new_word[1] =="r"
+      pr = @new_word[0, 2].join("")
+      spliced_word = @new_word[2..-1]
+      spliced_word << pr + "ay"
+    elsif @new_word[0] == "w" && @new_word[1] =="h"
+      wh = @new_word[0, 2].join("")
+      spliced_word = @new_word[2..-1]
+      spliced_word << wh + "ay"
+    elsif @new_word[0] == "s" && @new_word[1] =="k"
+      sk = @new_word[0, 2].join("")
+      spliced_word = @new_word[2..-1]
+      spliced_word << sk + "ay"
+    elsif (@new_word[0] == "s" || @new_word[0] == "S") && @new_word[1] =="t" && @new_word[2] == "r"
+      str = @new_word[0, 3].join("")
+      spliced_word = @new_word[3..-1]
+      spliced_word << str + "ay"
     else
-      word.concat(word.slice!(/^[^aeiouAEIOU]*/i || "")) + 'ay'
+      first_letter = @new_word[0]
+      spliced_word = @new_word[1..-1]
+      spliced_word << first_letter + "ay"
     end
   end
 
-
-  #if the word starts with a vowel add yay to the end
-  #if the word starts with consonant(s) add the consonants up to the next vowel and ay to the end
-  def to_pig_latin(phrase)
-    phrase.split.collect {|word| piglatinize(word)}.join(' ')
-	
-  end
 end
   
