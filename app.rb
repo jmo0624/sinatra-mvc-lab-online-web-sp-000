8297ae8defc6d4b91311013a622db20d7de92673
Josh Morris <jmo0624@gmail.com>
2020-02-29 19:50:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,21 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-   
+
   get '/' do
-    
     erb :user_input
-    
   end
-  
+
   post '/piglatinize' do
-    
-    user_word = params["user_phrase"]
-    word = PigLatinizer.new
-    @pig_latinized_word = word.to_pig_latin(user_word)
-    
-    erb :pig_latin
-  
+    translator = PigLatinizer.new
+    translator.to_pig_latin(params[:user_phrase])
   end
-  
 end
