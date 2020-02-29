945772e968a3614105f90f88323edf33b55632d5
Josh Morris <jmo0624@gmail.com>
2020-02-29 20:24:52 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,20 +1,22 @@
-def piglatinize (word)
-  exclude = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
-  vowels = ["a", "e", "i", "o", "u"]
+class PigLatinizer
+
+  def piglatinize (word)
+    exclude = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
+    vowels = ["a", "e", "i", "o", "u"]
     
-  if exclude.include?(word)
-    word
-  elsif vowels.include? word[0]
-    word << "ay"
-  else
-    consonants = ""
-  while !vowels.include?(word[0])
-    consonants << word[0]
-    word = word.split("")[1..-1].join
-  end
-    word + consonants + 'ay'
-  end
-  end
+    if exclude.include?(word)
+      word
+    elsif vowels.include? word[0]
+      word << "ay"
+    else
+      consonants = ""
+    while !vowels.include?(word[0])
+      consonants << word[0]
+      word = word.split("")[1..-1].join
+    end
+      word + consonants + 'ay'
+    end
+    end
 end	
 
 
