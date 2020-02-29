ef171b6507065ad8710d184e0cedc974c0289960
Josh Morris <jmo0624@gmail.com>
2020-02-29 20:12:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    translator = PigLatinizer.new
-    translator.to_pig_latin(params[:user_phrase])
+    @user_phrase = PigLatinizer.new.to_pig_latin(params[:user_phrase])
+    erb :result
   end
 end
