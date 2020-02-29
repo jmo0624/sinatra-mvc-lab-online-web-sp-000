443362613474d100d45e519f9c9ab2a0ffd006b1
Josh Morris <jmo0624@gmail.com>
2020-02-29 20:08:01 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,21 +1,34 @@
+
 class PigLatinizer
-  def to_pig_latin(phrase)
-    words = phrase.split(" ")
-    words.collect {|w| piglatinize(w)}.join(" ")
-  end
-  
-  def splits
+  attr_accessor :phrase	
+
+  def initialize(phrase)	
+    @phrase = phrase	
+  end	
+
+
+  def is_vowel?(word)
+    vowels = 'aeiouAEIOU'
+    vowels.include?(word[0])	
   end
 
-  def piglatinize(word)
-    parts_of_word = word.split(/([^aeiouAEIOU]*)([aeiouAEIOU]*)(.*)/)
-    # binding.pry
-    start = parts_of_word[1] # consonant cluster
-    rest = parts_of_word[2] + (parts_of_word[3] || "")
-    if start.length>0
-      "#{rest}#{start}ay"
-    else
-      "#{rest}way"
-    end
-  end
-end
+
+  def translate(word)	  def piglatinize(word)
+    if self.is_vowel?(word)	    if self.is_vowel?(word)
+      word.concat('way')	      word.concat('way')
+    else	    else
+      word.concat(word.slice!(/^[^aeiou]*/i || "")) + 'ay'	      word.concat(word.slice!(/^[^aeiouAEIOU]*/i || "")) + 'ay'
+    end	    end
+  end	  end
+
+
+  #if the word starts with a vowel add yay to the end	  #if the word starts with a vowel add yay to the end
+  #if the word starts with consonant(s) add the consonants up to the next vowel and ay to the end	  #if the word starts with consonant(s) add the consonants up to the next vowel and ay to the end
+  def translator	  def to_pig_latin(phrase)
+    words = @phrase.split	    phrase.split.collect {|word| piglatinize(word)}.join(' ')
+    new_string = words.map do |word|	
+      translate(word)	
+    end	
+    new_string.join(' ')	
+  end	  end
+end	end
