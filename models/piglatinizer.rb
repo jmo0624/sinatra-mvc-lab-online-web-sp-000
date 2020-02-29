dfcbaa1420c5276bd989601b331850abc180cdf4
Josh Morris <jmo0624@gmail.com>
2020-02-29 20:22:51 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,17 +1,16 @@
 def piglatinize (word)
-  if word.size == 1	
-    word	    exclude = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
-  elsif Vowels.include?(word[0])	    vowels = ["a", "e", "i", "o", "u"]
-    word + 'ay'	
-  elsif Consonants.include?(word[0]) && 	    if exclude.include?(word)
-    Consonants.include?(word[1])	      word
-    word[2..-1] + word[0..1] + 'ay'	    elsif vowels.include? word[0]
-  elsif word[0..1] == "qu" 	      word << "ay"
-    word[2..-1]+"quay"	    else
-  elsif word[0..2] == "squ"	      consonants = ""
-     word[3..-1]+"squay"	      while !vowels.include?(word[0])
-  else Consonants.include?(word[0])	        consonants << word[0]
-    word[1..-1] + word[0..0] + 'ay'	        word = word.split("")[1..-1].join
+  exclude = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
+  vowels = ["a", "e", "i", "o", "u"]
+    
+  if exclude.include?(word)
+    word
+  elsif vowels.include? word[0]
+    word << "ay"
+  else
+    consonants = ""
+  while !vowels.include?(word[0])
+    consonants << word[0]
+    word = word.split("")[1..-1].join
       end
       word + consonants + 'ay'
     end
