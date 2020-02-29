414ef3e7feb968c707f8a26ee9160d9a41877eff
Josh Morris <jmo0624@gmail.com>
2020-02-29 20:21:15 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,69 +1,26 @@
-class PigLatinizer
-
-  def piglatinize(user_phrase)
-    @new_word = user_phrase.split("")
-    vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
-    if vowels.include?(@new_word[0])
-      latinized_word = begins_with_vowel.join("")
-    else
-      latinized_word = begins_with_consonant.join("")
+def piglatinize (word)
+  if word.size == 1	
+    word	    exclude = ["i", "me", "to", "too", "a", "an", "in", "and", "on"]
+  elsif Vowels.include?(word[0])	    vowels = ["a", "e", "i", "o", "u"]
+    word + 'ay'	
+  elsif Consonants.include?(word[0]) && 	    if exclude.include?(word)
+    Consonants.include?(word[1])	      word
+    word[2..-1] + word[0..1] + 'ay'	    elsif vowels.include? word[0]
+  elsif word[0..1] == "qu" 	      word << "ay"
+    word[2..-1]+"quay"	    else
+  elsif word[0..2] == "squ"	      consonants = ""
+     word[3..-1]+"squay"	      while !vowels.include?(word[0])
+  else Consonants.include?(word[0])	        consonants << word[0]
+    word[1..-1] + word[0..0] + 'ay'	        word = word.split("")[1..-1].join
+      end
+      word + consonants + 'ay'
     end
-    latinized_word
-  end
+  end	  end
+end	
 
-  def to_pig_latin(sentence)
-    words_in_sentence = sentence.split(" ")
 
-    pig_latinized_words = words_in_sentence.collect do |word|
-      self.piglatinize(word)
-    end
-
-    new_sentence = pig_latinized_words.join(" ")
-  end
-
-  def begins_with_vowel
-    vowel_word = @new_word << "way"
-  end
-
-  def begins_with_consonant
-    if @new_word[0] == "q" && @new_word[1] == "u"
-      qu = @new_word[0, 2].join("")
-      spliced_word = @new_word[2..-1]
-      spliced_word << qa + "ay"
-    elsif @new_word[0] == "p" && @new_word[1] =="l"
-      pl = @new_word[0, 2].join("")
-      spliced_word = @new_word[2..-1]
-      spliced_word << pl + "ay"
-    elsif @new_word[0] == "t" && @new_word[1] =="h"
-      th = @new_word[0, 2].join("")
-      spliced_word = @new_word[2..-1]
-      spliced_word << th + "ay"
-    elsif @new_word[0] == "s" && @new_word[1] =="p" && @new_word[2] == "r"
-      spr = @new_word[0, 3].join("")
-      spliced_word = @new_word[3..-1]
-      spliced_word << spr + "ay"
-    elsif @new_word[0] == "p" && @new_word[1] =="r"
-      pr = @new_word[0, 2].join("")
-      spliced_word = @new_word[2..-1]
-      spliced_word << pr + "ay"
-    elsif @new_word[0] == "w" && @new_word[1] =="h"
-      wh = @new_word[0, 2].join("")
-      spliced_word = @new_word[2..-1]
-      spliced_word << wh + "ay"
-    elsif @new_word[0] == "s" && @new_word[1] =="k"
-      sk = @new_word[0, 2].join("")
-      spliced_word = @new_word[2..-1]
-      spliced_word << sk + "ay"
-    elsif (@new_word[0] == "s" || @new_word[0] == "S") && @new_word[1] =="t" && @new_word[2] == "r"
-      str = @new_word[0, 3].join("")
-      spliced_word = @new_word[3..-1]
-      spliced_word << str + "ay"
-    else
-      first_letter = @new_word[0]
-      spliced_word = @new_word[1..-1]
-      spliced_word << first_letter + "ay"
-    end
-  end
-
-end
+  def to_pig_latin (sentence)	  def to_pig_latin (sentence)
+    sentence.split.map(&method(:piglatinize)).join(' ')	    sentence.split.map{|word| piglatinize(word)}.join(' ')
+  end	  end
+end 	end 
   
